086cec61058188ac2083b0be2714100c081586e7
ChengYou <jolylai163@163.com>
2025-09-10 17:56:29 +0800

    feat: drizzle use snake case

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
-import type { Config } from 'drizzle-kit'
 import process from 'node:process'
+import { defineConfig } from 'drizzle-kit'
 import 'dotenv/config'
 
-export default {
+export default defineConfig({
   dialect: 'postgresql',
   schema: './db/schema/*',
   out: './db/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-} satisfies Config
+  casing: 'snake_case',
+})
